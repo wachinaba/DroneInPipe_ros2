faf4ac5e3214c51ccfa2ee60dd6d71d254c0cb82
wachinaba <wachinaba@gmail.com>
2021-08-23 02:06:12 +0900

    [Add] show PointEditor
    add statement to show PointEditor in fitting_visualizer_qt5.py

--- tools/fitting_visualizer_Qt5/fitting_visualizer_qt5.py
+++ tools/fitting_visualizer_Qt5/fitting_visualizer_qt5.py
@@ -2,9 +2,14 @@ import sys
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
+from point_editor import PointEditor
+
 class MainWindow(QMainWindow):
   def __init__(self):
     super().__init__()
+    self.points = []
+    self.editor = PointEditor(self.points)
+    self.setCentralWidget(self.editor)
 
 def main():
   app = QApplication(sys.argv)
