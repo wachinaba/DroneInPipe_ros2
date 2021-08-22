a36bcd635b19c22b327dbdca1aee0e917be6403b
wachinaba <wachinaba@gmail.com>
2021-08-23 02:05:16 +0900

    [Add] funcion of draw grid
    in class point_editor.PointEditor, function draw() now draws grid
    grid can be expand

--- tools/fitting_visualizer_Qt5/point_editor.py
+++ tools/fitting_visualizer_Qt5/point_editor.py
@@ -1,10 +1,13 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import QWaitCondition, Qt, QtCriticalMsg
+from PyQt5.QtCore import QWaitCondition, Qt, QtCriticalMsg, center
 
 class PointEditor(QtWidgets.QWidget):
   """
   Custom Qt Widget to edit points(scan data).
   """
+
+  GRID_SIZE = 100
+  SUBGRID_NUM = 5
 
   def __init__(self, points, *args, **kwargs):
     super(PointEditor, self).__init__(*args, **kwargs)
@@ -17,11 +20,58 @@ class PointEditor(QtWidgets.QWidget):
 
   def paintEvent(self, e):
     painter = QtGui.QPainter(self)
+
+    # fill background in lightGray
     brush_bg = QtGui.QBrush()
     brush_bg.setColor(QtGui.QColor("lightGray"))
     brush_bg.setStyle(Qt.SolidPattern)
     rect = QtCore.QRect(0, 0, painter.device().width(), painter.device().height())
     painter.fillRect(rect, brush_bg)
 
-  
+    #draw a grid
+    ##prepare brushes
+    pen_grid = QtGui.QPen()
+    pen_grid.setColor(QtGui.QColor("black"))
+    pen_subgrid = QtGui.QPen()
+    pen_subgrid.setColor(QtGui.QColor("gray"))
+
+    ##calc grid lines (a half number of all lines)
+    linenum_x = int(painter.device().width() / (self.GRID_SIZE*2))
+    linenum_y = int(painter.device().height() / (self.GRID_SIZE*2))
+
+    ##calc origin
+    center_x = painter.device().width() / 2
+    center_y = painter.device().height() / 2
+    origin_x = center_x - linenum_x * self.GRID_SIZE
+    origin_y = center_y - linenum_y * self.GRID_SIZE
+
+    print("{0}, {1}, {2}, {3}".format(center_x, center_y, origin_x, origin_y))
+
+    ##draw grid lines
+    ###vertical
+    for i in range(-1, linenum_x*2+1):
+      for j in range(self.SUBGRID_NUM):
+        displ = (i + j / self.SUBGRID_NUM) * self.GRID_SIZE
+        ql = QtCore.QLine(origin_x + displ, 0, origin_x + displ, painter.device().height())
+        if j == 0: #if main grid
+          painter.setPen(pen_grid)
+        else:
+          painter.setPen(pen_subgrid)
+        painter.drawLine(ql)
+
+    ###horizontal
+    for i in range(-1, linenum_y*2+1):
+      for j in range(self.SUBGRID_NUM):
+        displ = (i + j / self.SUBGRID_NUM) * self.GRID_SIZE
+        ql = QtCore.QLine(0, origin_y + displ, painter.device().width(), origin_y + displ)
+        if j == 0: #if main grid
+          painter.setPen(pen_grid)
+        else:
+          painter.setPen(pen_subgrid)
+        painter.drawLine(ql)
+
+      
+
+
+
 
