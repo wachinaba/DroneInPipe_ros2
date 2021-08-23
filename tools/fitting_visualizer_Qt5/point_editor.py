2da251933fc426ade0361f87efacabcf79c2be53
wachinaba <wachinaba@gmail.com>
2021-08-24 00:49:42 +0900

    [Change] replace grid drawing method
    replace grid drawing method to class-base

--- tools/fitting_visualizer_Qt5/point_editor.py
+++ tools/fitting_visualizer_Qt5/point_editor.py
@@ -54,7 +54,7 @@ class PointEditor(QtWidgets.QWidget):
       return QtCore.QSize(200, 200)
 
   def paintEvent(self, e):
-    painter = QtGui.QPainter(self)
+    painter = PointEditorPainter(self)
 
     # fill background in lightGray
     brush_bg = QtGui.QBrush()
@@ -63,37 +63,4 @@ class PointEditor(QtWidgets.QWidget):
     rect = QtCore.QRect(0, 0, painter.device().width(), painter.device().height())
     painter.fillRect(rect, brush_bg)
 
-    #draw a grid
-    ##prepare brushes
-    pen_grid = QtGui.QPen()
-    pen_grid.setColor(QtGui.QColor("black"))
-    pen_subgrid = QtGui.QPen()
-    pen_subgrid.setColor(QtGui.QColor("gray"))
-
-    ##calc grid lines (a half number of all lines)
-    linenum_x = int(painter.device().width() / (self.GRID_SIZE*2))
-    linenum_y = int(painter.device().height() / (self.GRID_SIZE*2))
-
-    ##calc origin
-    center_x = painter.device().width() / 2
-    center_y = painter.device().height() / 2
-    origin_x = center_x - linenum_x * self.GRID_SIZE
-    origin_y = center_y - linenum_y * self.GRID_SIZE
-
-    print("{0}, {1}, {2}, {3}".format(center_x, center_y, origin_x, origin_y))
-
-    ##draw grid lines
-    for j in range(self.SUBGRID_NUM):
-      if j == 0: #if main grid
-        painter.setPen(pen_grid)
-      else:
-        painter.setPen(pen_subgrid)
-      for i in range(-1, linenum_x*2+1):
-        displ = (i + j / self.SUBGRID_NUM) * self.GRID_SIZE
-        ql = QtCore.QLine(origin_x + displ, 0, origin_x + displ, painter.device().height())
-        painter.drawLine(ql)
-      for i in range(-1, linenum_y*2+1):
-        displ = (i + j / self.SUBGRID_NUM) * self.GRID_SIZE
-        ql = QtCore.QLine(0, origin_y + displ, painter.device().width(), origin_y + displ)
-        painter.drawLine(ql)
-    
+    painter.drawGrid(rect, GridStyle(self.GRID_SIZE, self.SUBGRID_NUM))
